58c4bbe820407d31992c18c7114e7e54393315c2
Aoxcis <gregoirepaul@laposte.net>
2025-03-10 09:46:31 +0100

    feat : exo1 jalon 1

---
+++ jalon1/exo01/index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Exo 01 - PHP</title>
+</head>
+<body>
+  <p>
+    <?php
+      echo date('H:i');;
+    ?>
+  </p>
+</body>
